066c5fff516896479060094b6a58f497f11776ae
Jorge Medina <jlmedinamacias@gmail.com>
2024-04-23 11:30:02 -0700

    Remove unused code

--- index.js
+++ index.js
@@ -1,32 +1,5 @@
 const { nextISSTimesForMyLocation } = require("./iss");
 
-/* fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  }
-
-  console.log("It worked! Returned IP:", ip);
-}); */
-
-/* fetchCoordsByIP('173.181.72.254', (error, coordinates) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  }
-
-  console.log("It worked! Returned coordinates:", coordinates);
-}); */
-
-/* fetchISSFlyOverTimes({ latitude: 49.2827291, longitude: -123.1207375 }, (error, times) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  }
-
-  console.log("It worked! Returned times:", times);
-}); */
-
 const printPassTimes = function (passTimes) {
   for (const pass of passTimes) {
     const datetime = new Date(0);
